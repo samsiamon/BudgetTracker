f928c9f0d9bbb031ccf83f90663b9ed2d1ce486f
Sam Siamon <sam.siamon@neudesic.com>
2023-04-16 13:02:51 -0700

    Initial Commit

---
+++ BudgetTracker/BudgetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp.swift
+//  BudgetTracker
+//
+//  Created by Sam.Siamon on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
