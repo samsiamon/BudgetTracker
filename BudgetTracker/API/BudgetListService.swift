bd99c58f39489036812b3426aaa37f15f96fa804
Sam Siamon <sam.siamon@neudesic.com>
2023-04-17 17:08:16 -0700

    added some api files

---
+++ BudgetTracker/API/BudgetListService.swift
@@ -0,0 +1,15 @@
+//
+//  BudgetListService.swift
+//  BudgetTracker
+//
+//  Created by Sam.Siamon on 4/17/23.
+//
+
+import Foundation
+
+class BudgetListService {
+    let baseURL = "/"
+
+    
+
+}
